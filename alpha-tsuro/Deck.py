b3eb07053263aeb56bc0ac46c130594de3ec8bbc
Timothy Gieseking <tgieseking2.71@gmail.com>
2018-05-16 19:51:01 -0400

    Implement deck creation from json

--- alpha-tsuro/Deck.py
+++ alpha-tsuro/Deck.py
@@ -1,2 +1,28 @@
+import json
+from Tile import Tile
+
 class Deck:
-	None
+	"""Stores the tiles in the deck."""
+	def __init__(self):
+		self.tiles = []
+
+	@classmethod
+	def from_json(cls, file_name):
+		"""Creates a deck from a json file.
+
+		The json file should be a list of objects. Each object should be a
+		permutation of the labels {"UL","UR","RU","RD","DR","DR","LD",LU}.
+		Objects that are not of this form are omitted.
+		"""
+		with open(file_name) as file:
+			tile_dict_list = json.load(file)
+		deck = Deck()
+		for tile_dict in tile_dict_list:
+			tile = Tile.from_dict(tile_dict)
+			if (tile != None):
+				deck.tiles.append(tile)
+		return deck
+
+
+testDeck = Deck.from_json("data/deckTiles.json")
+print [tile.connections for tile in testDeck.tiles]
--- alpha-tsuro/Tile.py
+++ alpha-tsuro/Tile.py
@@ -1,2 +1,37 @@
 class Tile:
-	None
+	"""A single tile.
+
+	Each tile has eight points on its border where its paths terminate.
+	These points are arranged as follows
+            UL    UR
+	    -----|----|-----
+	    |              |
+	  LU-              -RU
+	    |              |
+	  LD-              -RD
+	    |              |
+	    -----|----|-----
+	        DL    DR
+	The connections dict maps each point to where the path starting at the
+	point ends.
+	"""
+	def __init__(self):
+		self.connections = {"UL":None, "UR":None, "RU":None, "RD":None,
+							"DR":None, "DL":None, "LD":None, "LU":None}
+
+	@classmethod
+	def from_dict(cls, connections_dict):
+		"""Creates a file from a dict of the connections.
+
+		If the dict is a permutation of the points, then the corresponding
+		tile is returned. Otherwise, None is returned.
+		"""
+		tile = Tile()
+		try:
+			if (set(tile.connections.keys()) != set(connections_dict.keys()) or
+				set(tile.connections.keys()) != set(connections_dict.values())):
+				return None
+		except AttributeError:
+			return None
+		tile.connections = connections_dict
+		return tile
